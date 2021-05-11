8dc157b2427a6fb89ec27d09c147b0e7ead61b2d
blossomvn1 <55186741+blossomvn1@users.noreply.github.com>
2021-05-11 10:47:05 +0700

    install prettier

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const exphbs  = require('express-handlebars');
-const app = express()
-const port = 3005
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3005;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
-app.use(morgan('combined'))
-// template engine 
-app.engine('hbs', exphbs({
-    extname:'.hbs'
-}));
+app.use(express.json());
+app.use(morgan('combined'));
+// template engine
+app.engine(
+    'hbs',
+    exphbs({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname,'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'));
 
-
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     return res.render('home');
-})
-app.get('/news',(req,res)=>{
+});
+app.get('/news', (req, res) => {
     return res.render('news');
-})
-
-app.listen(port,() =>{console.log(`Server starting at port ${port}`)})
+});
+app.get('/search', (req, res) => {
+    return res.render('search');
+});
+app.post('/search', (req, res) => {
+    return res.render('');
+});
+app.listen(port, () => {
+    console.log(`Server starting at port ${port}`);
+});
