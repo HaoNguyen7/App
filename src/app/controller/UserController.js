a205fb12a0b5e96282a2fd3cdcfa804e151ba601
blossomvn1 <55186741+blossomvn1@users.noreply.github.com>
2021-05-27 21:27:21 +0700

    add edit profile page

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -1,12 +1,18 @@
-const Course = require('../models/course')
+const User = require('../models/user')
 const {multipleMongooseToObject,mongooseToObject} = require('../../util/mongoose')
 class UserController {
     //[GET] /news/:slug
    showDetail(req,res,next){
     res.render('user/user-detail')
 }
-
-   
+    editDetail(req,res,next){
+        res.render('user/user-detail-edit')
+    }
+    updateDetail(req,res,next){
+        User.updateOne({_id:req.params.id},req.body)
+            .then(()=>res.redirect(`/user/${req.params.id}`))
+            .catch(next)
+    }
 }
 
 module.exports = new UserController();
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,9 @@ const { requireAuth, checkUser } = require('../app/middlewares/authMiddleware');
 const userController = require('../app/controller/UserController');
 
 router.get('/:id',checkUser ,userController.showDetail);
+router.put('/:id/edit',checkUser,requireAuth ,userController.updateDetail);
+router.get('/:id/edit',checkUser ,userController.editDetail);
+
 
 
 module.exports = router;
